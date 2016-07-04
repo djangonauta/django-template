904ce2b3194acb4ea7a8f261e59afe9a05e0f125
igor <igor.dev.py@gmail.com>
2016-07-04 19:34:16 -0300

    Add default pagination.

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -185,6 +185,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 15,
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',
                                 'rest_framework.filters.SearchFilter',
