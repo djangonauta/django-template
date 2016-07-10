8b02ef5c4bfdf0d2f43cb53590f99fa280976b8d
igor <igor.dev.py@gmail.com>
2016-07-09 22:20:14 -0300

    Adicionado DisableSignupSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,7 @@
 """Serializadores da aplicação core."""
 
 from django.contrib.auth import get_user_model
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer, ValidationError
 
 
 class UserSerializer(ModelSerializer):
@@ -10,3 +10,11 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['id', 'username']
+
+
+class DisableSignupSerializer(Serializer):
+    """Serializador customizado."""
+
+    def validate(self, data):
+        """Desativa a criação de novas contas."""
+        raise ValidationError('Registro de novas contas temporariamente suspenso.')
