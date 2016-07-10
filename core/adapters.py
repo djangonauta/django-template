ad444980c2358b453b05bd740dc14931fe82ff0f
igor <igor.dev.py@gmail.com>
2016-07-09 22:06:11 -0300

    Adicionado allauth adapter.

---
+++ core/adapters.py
@@ -0,0 +1,11 @@
+"""Adaptadores para uso com a aplicação allauth."""
+
+from allauth.account.adapter import DefaultAccountAdapter
+
+
+class DisableSignupAdapter(DefaultAccountAdapter):
+    """Adaptador customizado."""
+
+    def is_open_for_signup(self, request):
+        """Desativa o processo de registro."""
+        return False
