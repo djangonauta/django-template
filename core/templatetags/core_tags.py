4fb669a5ac35c7f953b141bfd75f1a642aaa0cb5
igor <igor.dev.py@gmail.com>
2017-12-25 11:40:55 -0300

    fix: corrigida e adicionada templatetag

--- core/templatetags/core_tags.py
+++ core/templatetags/core_tags.py
@@ -1,13 +1,12 @@
 """Este módulo contem tags e filtros globais em relação ao projeto."""
 
-import hashlib
-
 from django import template
+from django.core import urlresolvers
 
 register = template.Library()
 
 
-@register.simple_tag
-def gravatar_url(email):
-    """Obtém a url gravatar em função do email fornecido."""
-    return '//www.gravatar.com/avatar/{}'.format(hashlib.md5(email.encode('utf-8')).hexdigest())
+@register.simple_tag(takes_context=True)
+def active(context, nome_url):
+    """Verifica se a url atual corresponde a url nomeada para gerar a classe ativa no metismenu."""
+    return 'active' if context['request'].get_full_path() == urlresolvers.reverse(nome_url) else ''
