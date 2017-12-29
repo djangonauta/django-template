46d34983aa9d633cab5a713c8da737b7e5e6f22b
igor <igor.dev.py@gmail.com>
2017-12-28 22:23:45 -0300

    fix: estilo de código

--- core/factories.py
+++ core/factories.py
@@ -8,6 +8,12 @@ from factory import django
 class UserFactory(django.DjangoModelFactory):
     """Fábrica de usuários."""
 
+    class Meta:
+        """Meta opções da fábrica."""
+
+        model = auth.get_user_model()
+        django_get_or_create = ('username', 'email')
+
     username = factory.Faker('first_name')
     password = factory.Faker('ean')
 
@@ -15,7 +21,3 @@ class UserFactory(django.DjangoModelFactory):
     def email(self):
         """Email aleatório."""
         return '{}@domain.com'.format(self.username)
-
-    class Meta:
-        model = auth.get_user_model()
-        django_get_or_create = ('username', 'email')
