722d21da3764f1655a0bd6bf69cfe35446657790
igor <igor.dev.py@gmail.com>
2018-03-01 17:21:32 -0300

    fix: corrigido assets uri

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
-STATIC_URL = '/assets/'
+STATIC_URL = '/public/'
 STATIC_ROOT = root.path('')('public')
 STATICFILES_DIRS = [root.path('{{ project_name }}')('assets'), root.path('')('node_modules')]
 STATICFILES_FINDERS = global_settings.STATICFILES_FINDERS + ['django_assets.finders.AssetsFinder']
