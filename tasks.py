e74f1b231ec3a9fae758312ef0aff71c7c049b51
Igor Carvalho <igbrch@gmail.com>
2022-09-24 14:09:36 -0300

    feat: adicionados mais parâmetros para as tarefas locais

--- tasks.py
+++ tasks.py
@@ -10,9 +10,16 @@ def run_server(c, noinput=True, clear=False, verbosity=0, settings='development'
 
 
 @invoke.task
-def test(c, flags='-Wa', package='', settings='test'):
-    cmd = f'python3 {flags} -m coverage run manage.py test {package} --settings=projeto.settings.{settings}'
+def test(c, flags='-Wa', package='', settings='test', parallel=True, keepdb=False):
+    args = []
+    args.append('--parallel' if parallel else '')
+    args.append('--keepdb' if keepdb else '')
+    args = ' '.join(args)
+
+    cmd = f'python3 {flags} -m coverage run manage.py test {package} --settings=projeto.settings.{settings} '
+    cmd += f'{args}'
     c.run(cmd, echo=True, pty=True)
+
     cmd = 'coverage report'
     c.run(cmd, echo=True, pty=True)
 
@@ -20,18 +27,23 @@ def test(c, flags='-Wa', package='', settings='test'):
 @invoke.task
 def functional_tests(c, package='functional_tests.histories', settings='test'):
     collectstatic(c, settings, True)
+
     cmd = f'coverage run ./manage.py test {package} . --settings=projeto.settings.{settings}'
     c.run(cmd, echo=True, pty=True)
+
     cmd = 'coverage report'
     c.run(cmd, echo=True, pty=True)
 
 
 @invoke.task
 def collectstatic(c, noinput=True, clear=False, verbosity=0, settings='development'):
-    noinput = '--noinput' if noinput else ''
-    clear = '--clear' if clear else ''
-    cmd = f'./manage.py collectstatic {noinput} {clear} --verbosity={verbosity} '
-    cmd += f'--settings=projeto.settings.{settings}'
+    args = []
+    args.append('--noinput' if noinput else '')
+    args.append('--clear' if clear else '')
+    args = ' '.join(args)
+
+    cmd = f'./manage.py collectstatic --verbosity={verbosity} --settings=projeto.settings.{settings} '
+    cmd += f'{args}'
     c.run(cmd, echo=True, pty=True)
 
 
