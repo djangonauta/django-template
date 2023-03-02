908563e1cd35dae5180b92774b24f89342942932
Igor Carvalho <igbrch@gmail.com>
2023-03-02 17:27:59 -0300

    feat: tarefa atualizada para email

--- projeto/apps/arquitetura/tasks.py
+++ projeto/apps/arquitetura/tasks.py
@@ -1,9 +1,11 @@
-import time
+from django.conf import settings
+from django.core import mail
 
 from projeto.celery import app
 
 
 @app.task
-def somar(*elementos):
-    time.sleep(5)
-    return sum(elementos)
+def email_admins_teste():
+    mail.send_mail('tarefa teste', 'Tarefa teste enviada via celery worker',
+                   from_email=settings.DEFAULT_FROM_EMAIL,
+                   recipient_list=dict(settings.ADMINS).values())
