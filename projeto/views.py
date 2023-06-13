829e04259a7bdedd1c24f6e172ffb74755e76456
djangonauta <igbrch@gmail.com>
2023-06-13 16:29:06 -0300

    feat: paginador

--- projeto/views.py
+++ projeto/views.py
@@ -1,10 +1,13 @@
 import view_breadcrumbs
+from auditlog.models import LogEntry
 from django import urls
 from django.contrib.auth import mixins
 from django.utils import functional
 from django.views import generic
 
+from projeto.apps.arquitetura.filters import QueryParamFilterSet
 from projeto.apps.arquitetura.mixins import BaseReportResponseMixin
+from projeto.apps.arquitetura.views import ElidedListView
 
 
 class IndexView(generic.TemplateView):
@@ -15,9 +18,21 @@ class IndexView(generic.TemplateView):
 index = IndexView.as_view()
 
 
-class AppView(mixins.LoginRequiredMixin, view_breadcrumbs.BaseBreadcrumbMixin, generic.TemplateView):
+class LogEntryFilter(QueryParamFilterSet):
+
+    class Meta:
+        model = LogEntry
+        fields = ['object_repr', 'action']
+
+
+class AppView(mixins.LoginRequiredMixin, view_breadcrumbs.BaseBreadcrumbMixin, ElidedListView):
 
     template_name = 'app.html'
+    queryset = LogEntry.objects.all()
+    context_object_name = 'entries'
+    paginate_by = 4
+    page_kwarg = 'pagina'
+    filter_class = LogEntryFilter
 
     @functional.cached_property
     def crumbs(self):
